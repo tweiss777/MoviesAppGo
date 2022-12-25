baa46f469eca23220c1869a78a6af792e1a207cb
Tal Weiss <Tweiss747@gmail.com>
2022-12-25 22:39:27 +0200

    added useNavigate

--- client/src/context/AuthenticationContext.tsx
+++ client/src/context/AuthenticationContext.tsx
@@ -1,21 +1,26 @@
 import React, { createContext, useEffect, useState } from "react";
-
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 //Authentication context interface
 interface IAuthenticationContext {
  login: Function,
  logout: Function,
  isAdmin: boolean
+ authErrors: boolean
  isAuthenticated: boolean 
  isAuthenticating: boolean
 }
 
-
+const users: any []= [
+  { username: "admin", password: "abc123"}
+];
 
 
 //Authentication context default values
 const AuthenticationContextDefaultValues: IAuthenticationContext = {
   login: () =>  console.log("logging in"),
   logout: () => console.log("logging out"),
+  authErrors: false,
   isAuthenticated: false,
   isAuthenticating: false,
   isAdmin: false
@@ -31,19 +36,35 @@ interface IProps{
 }
 
 export default function AuthenticationProvider({children}: IProps){
+  
+  // authentication states
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
   const [isAuthenticating, setIsAuthenticating] = useState<boolean>(false)  
   const [isAdmin, setIsAdmin] = useState<boolean>(false)
-  
-  // useEffect()
+  const [authErrors, setAuthErrors] = useState<boolean>(false)
+  const [cookies, setCookie, removeCookie] = useCookies(['authenticated'])
+  const navigate = useNavigate()
+
+
+  useEffect(() => {
+    console.log('checking if user is authenticated...')
+    if(cookies['authenticated']){
+      console.log('redirecting to dashboard')
+    } else {
+      navigate('/login')
+    }
+  })
 
 
   // use this to handle login
   const login: Function = (userName: string, password: string) => {
     console.log("loggging user in")
-    setTimeout(() => console.log(`user ${userName} logged in succesfully`),3000)
-    console.log(`username ${userName}`)
-    console.log(`password ${password}`)
+    const user = users.find(user => user.username === userName.toLowerCase() && user.password === password.toLowerCase())
+    if(user){
+      setCookie('authenticated', 'true')
+    } else {
+
+    }
     
   }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { CookiesProvider } from 'react-cookie'
 import './index.css'
 import { router } from './routes/router'
 import { RouterProvider } from 'react-router-dom'
@@ -8,8 +8,10 @@ import AuthenticationProvider from './context/AuthenticationContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AuthenticationProvider>
-      <RouterProvider router={router} />
-    </AuthenticationProvider>
+    <CookiesProvider>
+      <AuthenticationProvider>
+        <RouterProvider router={router} />
+      </AuthenticationProvider>
+    </CookiesProvider>
   </React.StrictMode>,
 )
