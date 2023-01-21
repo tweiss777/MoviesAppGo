72902180a53082fff58acfb2b26be0487a48ad0f
Tal Weiss <Tweiss747@gmail.com>
2023-01-21 11:30:28 +0200

    Added try catch to dashboard

--- client/src/data/movies.ts
+++ client/src/data/movies.ts
@@ -2,8 +2,13 @@ import { IMovie } from "./IMovie";
 import axios from "axios";
 
 export async function getMovies(): Promise<IMovie[]>{
-  const { data } = await axios.get('/v1/movies/get-movies')
-  return data
+  try{
+    const { data } = await axios.get('/v1/movies/get-movies')
+    return data
+  } catch(err){
+    throw new Error(err as string)
+  }
+ 
 }
 
 export async function addMovie(movie:IMovie){
